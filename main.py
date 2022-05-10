03b221f292a5052b3d4aba61d17bdc9032f30083
Roover <93544398+RooverPY@users.noreply.github.com>
2022-05-10 12:13:28 -0400

    Version 2.0
    - Added ability to save invites
    - Invite info

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ WHITE = '\033[0m'
 BOLD = '\033[1m'
 CYAN = '\033[96m'
 chars = []
+valid = []
 chars[:0] = string.ascii_letters + string.digits
 if os.stat("proxies.txt").st_size > 0:
 	global proxies
@@ -22,18 +23,23 @@ else:
 	print(f"{RED}	Proxies are required")
 	quit()
 
-async def gen(amount: int):
+async def gen(amount: int, log: bool):
 	async with aiohttp.ClientSession() as session:
 		prox = False
 		for i in range(amount):
 			code = "".join(random.choices(chars, k=8))
 			if prox is False:
-				check = await session.get(f"https://discord.com/api/v9/invites/{code}?with_counts=true&with_expiration=true",headers=headers)
+				check = await session.get(f"https://discord.com/api/v9/invites/obama?with_counts=true&with_expiration=true",headers=headers)
 			else:
 				check = await session.get(f"https://discord.com/api/v9/invites/{code}?with_counts=true&with_expiration=true",proxy=f"http://{next(proxies)}", headers=headers)
 
 			if check.status == 200:
-				print(f"{GREEN}	Valid {i} : {code}")	
+				json = await check.json()
+				count = str(json["approximate_member_count"])
+				name = json["guild"]["name"]
+				print(f"{GREEN}	Valid {i} : {code} : {name} : {count}")
+				if log == True:
+					valid.append(code + " : Name: " + name + " |  Member Count: " + count)
 			elif check.status == 429:
 				print(f"{BLUE}	Ratelimited {i+1} :{code}")		
 				prox = True
@@ -52,15 +58,19 @@ def start():
 		  """)
 	print(CYAN + '	A powerful tool to generate, check and store Discord Invites ✉️\n')
 	while True:
-		try:
 			amount = int(input(GREEN + "	How many invites do you want to generate? "))
+			save = input(GREEN + "	Would you like to save valid invites (y/n)? ")
+			log = False
+			if save == "y":
+				log = True
 			print("\n")
 			break
-		except Exception as e:
-			print(f"	~ {YELLOW}Input Failure | {e}")	
-			pass
-	asyncio.run(gen(amount))
-
+	asyncio.run(gen(amount, log))
+	if log == True:
+		with open("data.txt", "a+") as data:
+			for code in valid:
+				data.write(code + "\n")
+	
 if __name__ == "__main__":
 	os.system("cls" if os.name == "nt" else "clear")
 	start()
